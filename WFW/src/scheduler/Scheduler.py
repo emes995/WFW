2a18ea02891f8699d55afbe39507274d34d09a65
ecarrera <edgar.carrera@outlook.com>
2019-10-28 19:46:49 -0400

    Async version

--- WFW/src/scheduler/Scheduler.py
+++ WFW/src/scheduler/Scheduler.py
@@ -7,14 +7,14 @@ class Scheduler:
         self._tasks = []
         self._dependency_manager = dependency_manager
 
-    def add_task(self, task: Task):
+    async def add_task(self, task: Task):
         self._tasks.append(task)
 
-    def execute_tasks(self):
+    async def execute_tasks(self):
         for _t in self._tasks:
             _tasks = _t.resolve_dependencies()
             for _dt in _tasks:
-                _dt.do_work()
-            _t.do_work()
+                await _dt.do_work()
+            await _t.do_work()
 
         return self
